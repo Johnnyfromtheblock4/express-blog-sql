8c8204464bbde1e90e5cb866eecb284fdad603aa
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-17 18:59:38 +0200

    [UPD] milestone 1

---
+++ data/db.js
@@ -0,0 +1,22 @@
+// importo mysql2
+const mysql = require("mysql2");
+
+// creo la connessione al db
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_post",
+});
+
+// instauro una connessione al database
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to mysql");
+  }
+});
+
+// esportiamo la connessione creata
+module.exports = connection
